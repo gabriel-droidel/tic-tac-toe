b4032e601bb3605066a8c6f4f9eea5231d0b7812
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-02-04 10:59:48 +0100

    Finish console game
    Now the game runs properly in the console
    I have impleneted IIFEs and factory functions
    All main functionalities are nested inside objects
    created with factory functions

--- code.js
+++ code.js
@@ -1,110 +1,143 @@
-function generateGameBoard() {
-	const gameBoard = (function () {
-		//generate locations for the tic tac toe board.
-		const populate = () => Array(9).fill(null);
+// Game board //
+function createGameBoard() {
+	const board = [];
+	//generate all the values for the board
+	for (let i = 0; i < 9; i++) {
+		board.push(null);
+	}
+	// get the board values when needed
+	const getBoard = () => board;
 
-		// draw the board in the console
-		const draw = (array) => {
-			console.log(`
+	// markCell
+	const markCell = (player, selection) => {
+		let location;
+		do {
+			location = Number(
+				prompt(`${player} (${selection}), choose your location (0-8):`)
+			);
+		} while (board[location] !== null); // Keep asking if location is taken
+
+		board[location] = selection;
+	};
+
+	// draw the board in the console
+	const drawBoard = (array) => {
+		console.log(`
 		${array[0]} _ ${array[1]} _ ${array[2]} 
 		${array[3]} _ ${array[4]} _ ${array[5]}
 		${array[6]} _ ${array[7]} _ ${array[8]}
 		`);
-		};
-		return { populate, draw };
-	})();
-	// create board object
-	const values = gameBoard.populate();
-	const draw = () => gameBoard.draw(values);
+	};
 
-	return { values, draw };
+	return { getBoard, drawBoard, markCell };
 }
-// User //
-function createUser() {
-	const name = prompt('Player name : ');
-	const selection = prompt('Choose X or O').toUpperCase();
+// Player //
+function createPlayers() {
+	const players = [];
+	const createPlayer = () => {
+		const name = prompt('Player name : ');
+		const selection = prompt('Choose X or O').toUpperCase();
+		return { name, selection };
+	};
+	const create = () => {
+		const playerOne = createPlayer();
+		const playerTwo = createPlayer();
+		players.push(playerOne, playerTwo);
+		return players;
+	};
+	const getPlayers = () => players;
 
-	const getName = () => name;
-	const getSelection = () => selection;
-	return { getName, getSelection };
-}
-
-function checkLocations(gameBoard) {
-	// check if the grid is full
-	return gameBoard.every((element) => element === 'X' || element === 'O');
+	return { create, getPlayers };
 }
 
 // Game //
-function createGame() {
-	const board = generateGameBoard();
-	let finished = false;
-	const players = [];
-	const playerOne = createUser();
-	players.push(playerOne);
-	const playerTwo = createUser();
-	players.push(playerTwo);
+function createGameController(
+	playerOneName = 'PlayerOne',
+	playerTwoName = 'PlayerTwo'
+) {
+	const board = createGameBoard();
+	const players = createPlayers();
+	players.create();
+	let activePlayer = players.getPlayers()[0];
 
-	return { board, players, finished };
+	const switchPlayerTurn = () => {
+		activePlayer =
+			activePlayer === players.getPlayers()[0]
+				? players.getPlayers()[1]
+				: players.getPlayers()[0];
+	};
+	const getActivePlayer = () => activePlayer;
+
+	const handleWinLogic = (values, selection) => {
+		// Ensure cells are filled with the same player's selection
+		const rowWin =
+			(values[0] === selection &&
+				values[1] === selection &&
+				values[2] === selection) ||
+			(values[3] === selection &&
+				values[4] === selection &&
+				values[5] === selection) ||
+			(values[6] === selection &&
+				values[7] === selection &&
+				values[8] === selection);
+
+		const colWin =
+			(values[0] === selection &&
+				values[3] === selection &&
+				values[6] === selection) ||
+			(values[1] === selection &&
+				values[4] === selection &&
+				values[7] === selection) ||
+			(values[2] === selection &&
+				values[5] === selection &&
+				values[8] === selection);
+
+		const diagWin =
+			(values[0] === selection &&
+				values[4] === selection &&
+				values[8] === selection) ||
+			(values[2] === selection &&
+				values[4] === selection &&
+				values[6] === selection);
+
+		if (rowWin || colWin || diagWin) {
+			return true;
+		}
+
+		return false;
+	};
+
+	const playRound = () => {
+		board.drawBoard(board.getBoard());
+		let gameOver = false;
+		let moves = 0;
+
+		while (!gameOver && moves < 9) {
+			board.markCell(getActivePlayer().name, getActivePlayer().selection);
+			board.drawBoard(board.getBoard());
+			if (handleWinLogic(board.getBoard(), getActivePlayer().selection)) {
+				gameOver = true;
+				break;
+			}
+			switchPlayerTurn();
+			moves++;
+		}
+		if (gameOver === true) return getActivePlayer().name;
+		else return "It's a tie!";
+	};
+
+	return { playRound };
 }
 
-function handleWinLogic(board, player) {
-    const values = board.values;
-    const symbol = player.getSelection();
-
-    // Ensure cells are filled with the same player's symbol
-    const rowWin =
-        (values[0] === symbol && values[1] === symbol && values[2] === symbol) ||
-        (values[3] === symbol && values[4] === symbol && values[5] === symbol) ||
-        (values[6] === symbol && values[7] === symbol && values[8] === symbol);
-
-    const colWin =
-        (values[0] === symbol && values[3] === symbol && values[6] === symbol) ||
-        (values[1] === symbol && values[4] === symbol && values[7] === symbol) ||
-        (values[2] === symbol && values[5] === symbol && values[8] === symbol);
-
-    const diagWin =
-        (values[0] === symbol && values[4] === symbol && values[8] === symbol) ||
-        (values[2] === symbol && values[4] === symbol && values[6] === symbol);
-
-    if (rowWin || colWin || diagWin) {
-        console.log(`${player.getName()} (${symbol}) won!`);
-        return true;
-    }
-
-    return false;
+function drawGameBoard(board) {
+	const pageContainer = document.querySelector('.game-container');
+	pageContainer.textContent = '';
+	console.log(board);
+	board.forEach((item) => {
+		const box = document.createElement('div');
+		box.classList.add('selection-box');
+		pageContainer.appendChild(box);
+	});
 }
 
-function startGame() {
-    const gameData = createGame();
-    gameData.board.draw();
-
-    let currentPlayerIndex = 0;
-    let moves = 0;
-    let gameOver = false;
-
-    while (moves < 9 && !gameOver) {
-        assignPlayerLocation(gameData.board, gameData.players[currentPlayerIndex]);
-        gameData.board.draw();
-
-        if (handleWinLogic(gameData.board, gameData.players[currentPlayerIndex])) {
-            gameOver = true;
-            break;
-        }
-
-        currentPlayerIndex = 1 - currentPlayerIndex; // Switch players
-        moves++;
-    }
-
-    if (!gameOver) {
-        console.log("It's a draw!");
-    }
-}
-
-function assignPlayerLocation(board, player) {
-    let location;
-    do {
-        location = Number(prompt(`${player.getName()} (${player.getSelection()}), choose your location (0-8):`));
-    } while (board.values[location] !== null); // Keep asking if location is taken
-
-    board.values[location] = player.getSelection();
-}
+function placeMarkerOnDOM(container) {}
